f8e5683a04e61f64706670514a35e9f4418d67d4
Frael <frael.o1805@gmail.com>
2022-12-29 23:09:52 -0500

    commitendo desde otra cuenta

--- src/view/Login.js
+++ src/view/Login.js
@@ -4,19 +4,33 @@ import React, { useState } from "react";
 export const Login = () => {
 
     const [usuario, setUsuario] = useState('')
-    
+    const [contrasenia, setContrasenia] = useState('');
+
+    const handleUsuario = (e) => {
+        setUsuario(e.target.value)
+    }
+
+    const handleContrasenia = (e) => {
+        setContrasenia(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log({usuario, contrasenia})
+    }
+
     return (
 
         <Row className="my-4 justify-content-center">
             <Col md='4'>
-                <Form className="card card-body">
+                <Form className="card card-body" onSubmit={handleSubmit} >
                     <Form.Floating className="mb-3">
-                        <Form.Control placeholder="Usuario" ></Form.Control>
+                        <Form.Control onChange={handleUsuario} placeholder="Usuario" ></Form.Control>
                         <Form.Label>Usuario</Form.Label>
                     </Form.Floating>
 
                     <Form.Floating className="mb-3">
-                        <Form.Control placeholder="Contraseñ" type="password"></Form.Control>
+                        <Form.Control onChange={handleContrasenia} placeholder="Contraseñ" type="password"></Form.Control>
                         <Form.Label>Contraseña</Form.Label>
                     </Form.Floating>
 
